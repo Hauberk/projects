e947ee8b9d11bfd0c64745612af0a1665262656d
Abhishek Kumar <36488174+Hauberk@users.noreply.github.com>
2018-03-07 02:18:48 +0530

    Add files via upload
    This is the code for Factorial, Input is given through ajax(asynchronous javascript)

---
+++ input_factorial.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>
+Factorial
+</title>
+<script src = jquery.js></script>
+
+<script src = fact.js></script>
+</head>
+<body>
+<div>
+Input The text Here to check if it is palindrome:<br><input type="number" id="num1" placeholder="input Number"><br>
+<button id="sub">calculate</button><br>
+<textarea id="response" style="width:200px; height:100px; resize:none;"></textarea>
+
+</div>
+
+
+</body>
+</html>
